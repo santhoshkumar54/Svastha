afc037be1e22e6839ea4b48924c6b807a2fa8766
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2025-01-19 23:37:18 +0530

    route master apis (#5)

--- svastha/src/main/java/com/svastha/controller/RouteController.java
+++ svastha/src/main/java/com/svastha/controller/RouteController.java
@@ -1,11 +1,23 @@
 package com.svastha.controller;
 
+import com.svastha.entity.RouteMaster;
+import com.svastha.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.svastha.service.RouteService;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @RestController
 public class RouteController {
@@ -22,6 +34,40 @@ public class RouteController {
 	public void villageLocationMapper(@RequestParam Long thalukId) {
 		routes.villageLocationMapper(thalukId);
 	}
-	
-	
+
+	@GetMapping("/routes")
+	public Page<RouteMaster> getRoutes(
+			@RequestParam(required = false) String routeName,
+			@RequestParam(required = false) Integer count,
+			@RequestParam(required = false) Long assignedTo,
+			@RequestParam(required = false) Long assignedBy,
+			@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd")
+			LocalDate startDate,
+			@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd")
+			LocalDate endDate,
+			Pageable pageable) {
+
+		Timestamp startTimestamp = (startDate != null)
+				? Timestamp.valueOf(startDate.atTime(LocalTime.MIN)) // Appending 00:00:00 time to date
+				: null;
+		Timestamp endTimestamp = (endDate != null)
+				? Timestamp.valueOf(endDate.atTime(LocalTime.MAX)) // Appends 23:59:59.999999 time to date
+				: null;
+		return routes.getRoutesWithFilters(routeName, count, assignedTo, assignedBy, startTimestamp , endTimestamp, pageable);
+	}
+
+	@GetMapping("/getRoute/{routeId}")
+	public @ResponseBody  RouteMaster getRouteById(@PathVariable Long routeId) {
+		return routes.getRouteById(routeId);
+	}
+
+	@PostMapping("/saveRoute")
+	public @ResponseBody RouteMaster createRoute(@RequestBody RouteMaster route) {
+		return routes.createRoute(route);
+	}
+
+	@DeleteMapping("/deleteRoute")
+	public void deleteRoute(@RequestBody RouteMaster route) {
+		routes.deleteRoute(route);
+	}
 }
--- svastha/src/main/java/com/svastha/repository/RouteMasterRepository.java
+++ svastha/src/main/java/com/svastha/repository/RouteMasterRepository.java
@@ -1,9 +1,31 @@
 package com.svastha.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.svastha.entity.RouteMaster;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
 
 public interface RouteMasterRepository extends JpaRepository<RouteMaster, Long> {
+
+
+    @Query("SELECT r FROM RouteMaster r " +
+            "WHERE (:routeName IS NULL OR r.routeName LIKE %:routeName%) " +
+            "AND (:count IS NULL OR r.count = :count) " +
+            "AND (:assignedTo IS NULL OR r.assignedTo.pk1 = :assignedTo) " +
+            "AND (:assignedBy IS NULL OR r.assignedBy.pk1 = :assignedBy) " +
+            "AND (:startDate IS NULL OR r.assignedDate >= :startDate) " +
+            "AND (:endDate IS NULL OR r.assignedDate <= :endDate)")
+    Page<RouteMaster> findWithFilters(
+            @Param("routeName") String routeName,
+            @Param("count") Integer count,
+            @Param("assignedTo") Long assignedTo,
+            @Param("assignedBy") Long assignedBy,
+            @Param("startDate") Timestamp startDate,
+            @Param("endDate") Timestamp endDate,
+            Pageable pageable);
 
 }
